5ca2082243ed647470a9b489631664b5f9d196ec
Boris Gjenero <boris.gjenero@gmail.com>
2013-12-04 00:08:01 -0500

    Add tool for dumping raw font in user-friendly format.
    This creates a LED sign tools directory. There is no Makefile
    needed when tools are trivial to build and rarely used.

---
+++ ledsign/tools/dumprawfont.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+# include <io.h>
+# include <fcntl.h>
+# define SET_BINARY_MODE(handle) setmode(handle, O_BINARY)
+#else
+# define SET_BINARY_MODE(handle) ((void)0)
+#endif
+
+int main(void) {
+    int ascii = 0;
+    int line = 0;
+
+    SET_BINARY_MODE(stdin);
+    while (!feof(stdin)) {
+        unsigned char i;
+        int c = getchar();
+        if (c < 0) break;
+        printf("%03i,%i: ", ascii, line);
+        for (i = 0x80; i != 0; i >>= 1) {
+            if (c & i) {
+                putchar('*');
+            } else {
+                putchar(' ');
+            }
+        }
+        printf("\n");
+        line++;
+        if (line == 7) {
+            line = 0;
+            ascii++;
+            printf("\n");
+        }
+    }
+    return 0;
+}
