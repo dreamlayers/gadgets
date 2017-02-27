dreamlayers/gadgets

commit d9baa976f531dd708a1909fc29386ff289853729

Update RGB lamp visualization plugin for Audacious 3.6.2

rgblamp/vis_rgb/aud_rgb.cc
@@ -1,0 +1,60 @@
+/* Audacious-specific visualization plugin code for the RGB lamp. */
+/* Copyright 2013 Boris Gjenero. Released under the MIT license. */
+
+#include <stdio.h>
+
+#include <glib.h>
+#include <libaudcore/i18n.h>
+#define AUD_PLUGIN_GLIB_ONLY
+#include <libaudcore/plugin.h>
+#include "rgbm.h"
+
+class ARGB : public VisPlugin
+{
+public:
+    static const char about[];
+
+    static constexpr PluginInfo info = {
+        N_("Audacious RGB"),
+        N_("gadgets"),
+        about
+    };
+
+    constexpr ARGB () : VisPlugin (info, Visualizer::MonoPCM) {}
+
+    bool init ();
+    void cleanup ();
+
+    void render_mono_pcm (const float * pcm);
+    void clear ();
+};
+
+
+__attribute__((visibility("default"))) ARGB aud_plugin_instance;
+
+gboolean plugin_is_active = FALSE;
+
+const char ARGB::about[] =
+ N_("Audacious RGB\n");
+
+bool ARGB::init(void)
+{
+    int i = rgbm_init();
+    printf("init %i\n", i);
+    return i;
+}
+
+void ARGB::cleanup(void)
+{
+    rgbm_shutdown();
+}
+
+void ARGB::render_mono_pcm(const gfloat *freq)
+{
+    rgbm_render(freq);
+}
+
+void ARGB::clear()
+{
+    // Not sure if ignoring this is the best strategy
+}

rgblamp/vis_rgb/Makefile
@@ -40,12 +40,13 @@ rgbm.o: greentab_audacious.h freqadj_audacious.h
 
 endif
 
-CFLAGS := $(CFLAGS)
+CXXFLAGS := $(CFLAGS) -std=c++11
 
 OBJS := $(SRCS:%.c=%.o)
+OBJS := $(OBJS:%.cc=%.o)
 
 $(TARGET): $(OBJS) $(LIBRGBLAMP_DIR)/librgblamp.a
-	$(CC) $(CFLAGS) -shared -Wl,--no-undefined \
+	$(CXX) $(CXXFLAGS) -shared -Wl,--no-undefined \
 	-Wl,--exclude-libs,ALL $^ $(LIBS) -o $@
 
 .PHONY : clean veryclean
@@ -57,7 +58,7 @@ veryclean: clean
 
 rgbvis.o: rgbvis.c $(WINAMPAPI_DIR)/vis.h rgbm.h Makefile
 
-aud_rgb.o: aud_rgb.c rgbm.h Makefile
+aud_rgb.o: aud_rgb.cc rgbm.h Makefile
 
 rgbm.o: rgbm.c rgbm.h $(LIBRGBLAMP_DIR)/librgblamp.h Makefile \
 

rgblamp/vis_rgb/aud_rgb.c
@@ -1,47 +1,0 @@
-/* Audacious-specific visualization plugin code for the RGB lamp. */
-/* Copyright 2013 Boris Gjenero. Released under the MIT license. */
-
-#include <glib.h>
-#include <audacious/plugin.h>
-#include "rgbm.h"
-
-static bool_t
-rgblamp_init(void)
-{
-    return rgbm_init();
-}
-
-static void
-rgblamp_cleanup(void)
-{
-    rgbm_shutdown();
-}
-
-static void
-rgblamp_render_freq(const gfloat *freq)
-{
-    rgbm_render(freq);
-}
-
-#if defined(__GNUC__) && __GNUC__ >= 4
-#define HAVE_VISIBILITY 1
-#else
-#define HAVE_VISIBILITY 0
-#endif
-
-#if HAVE_VISIBILITY
-#pragma GCC visibility push(default)
-#endif
-
-AUD_VIS_PLUGIN(
-    .name = "RGB Lamp",
-    .init = rgblamp_init,
-    .cleanup = rgblamp_cleanup,
-    //.about = aosd_about,
-    //.configure = rgblamp_configure,
-    .render_freq = rgblamp_render_freq,
-)
-
-#if HAVE_VISIBILITY
-#pragma GCC visibility pop
-#endif

commit 40fb6398d71f2177a233587c4686c42e6fe00332

Make Audacious RGB lamp plugin use frequencies, not the waveform
This was a very stupid bug introduced in d9baa97 when updating
to the new C++ plugin API for Audacious 3.6.2.

rgblamp/vis_rgb/aud_rgb.cc
@@ -1,8 +1,6 @@
 /* Audacious-specific visualization plugin code for the RGB lamp. */
 /* Copyright 2013 Boris Gjenero. Released under the MIT license. */
 
-#include <stdio.h>
-
 #include <glib.h>
 #include <libaudcore/i18n.h>
 #define AUD_PLUGIN_GLIB_ONLY
@@ -20,12 +18,13 @@ public:
         about
     };
 
-    constexpr ARGB () : VisPlugin (info, Visualizer::MonoPCM) {}
+    constexpr ARGB () : VisPlugin (info, Visualizer::Freq) {}
 
     bool init ();
     void cleanup ();
 
-    void render_mono_pcm (const float * pcm);
+    /* intensity of frequencies 1/512, 2/512, ..., 256/512 of sample rate */
+    void render_freq (const float * freq);
     void clear ();
 };
 
@@ -40,7 +39,6 @@ const char ARGB::about[] =
 bool ARGB::init(void)
 {
     int i = rgbm_init();
-    printf("init %i\n", i);
     return i;
 }
 
@@ -49,7 +47,7 @@ void ARGB::cleanup(void)
     rgbm_shutdown();
 }
 
-void ARGB::render_mono_pcm(const gfloat *freq)
+void ARGB::render_freq(const gfloat *freq)
 {
     rgbm_render(freq);
 }