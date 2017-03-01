b896c2bd5f39ccf46b76dcac2e4a9b02a108addc
Boris Gjenero <boris.gjenero@gmail.com>
2017-03-01 14:17:28 -0500

    Make SDL RGB lamp visualizer window resizeable

--- rgblamp/vis_rgb/sdl_rgb.c
+++ rgblamp/vis_rgb/sdl_rgb.c
@@ -6,8 +6,10 @@
 #include <SDL.h>
 #include "librgblamp.h"
 
-#define width 640
-#define height 480
+
+static unsigned int savedr = 0, savedg = 0, savedb = 0;
+static unsigned width, height;
+static SDL_Surface *screen = NULL;
 
 static void sdlError(const char *str)
 {
@@ -15,7 +17,34 @@ static void sdlError(const char *str)
     fprintf(stderr,"(reason for error: %s)\n", SDL_GetError());
 }
 
-static SDL_Surface *screen;
+static bool redraw(void) {
+    if SDL_MUSTLOCK(screen) {
+        SDL_LockSurface(screen);
+    }
+    SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format,
+                                          savedr, savedg, savedb));
+    if SDL_MUSTLOCK(screen) {
+        SDL_UnlockSurface(screen);
+    }
+    SDL_UpdateRect(screen, 0, 0, width, height);
+    return true; // TODO: error handling
+}
+
+static bool screen_init(unsigned int newwidth, unsigned int newheight) {
+    width = newwidth;
+    height = newheight;
+
+    screen = SDL_SetVideoMode(width, height, 0, SDL_HWSURFACE | SDL_RESIZABLE);
+
+    SDL_WM_SetCaption("RGB lamp visualizer", "RGB lamp visualizer");
+
+    if (!screen) {
+        sdlError("setting video mode");
+        return false;
+    } else {
+        return redraw();
+    }
+}
 
 RGBAPI bool rgb_open(const char *fn) {
     (void)fn;
@@ -25,16 +54,7 @@ RGBAPI bool rgb_open(const char *fn) {
         return false;
     }
 
-    SDL_WM_SetCaption("RGB lamp visualizer", "RGB lamp visualizer");
-
-    screen = SDL_SetVideoMode(width, height, 0, SDL_HWSURFACE);
-
-    if (!screen) {
-        sdlError("setting video mode");
-        return false;
-    }
-
-    return true;
+    return screen_init(640, 480);
 }
 
 static unsigned int pwm2srgb256(unsigned short n) {
@@ -53,25 +73,35 @@ static unsigned int pwm2srgb256(unsigned short n) {
     return r * 255.0;
 }
 
-
 static bool pollevents(void) {
     SDL_Event event;
-    return SDL_PollEvent(&event) != 0 && event.type == SDL_QUIT;
+    while (SDL_PollEvent(&event) > 0) {
+        switch (event.type) {
+        case SDL_VIDEOEXPOSE:
+            if (!redraw()) return false;
+            break;
+        case SDL_VIDEORESIZE:
+            if (width != (event.resize.w) || height != (event.resize.h)) {
+                if (!screen_init(event.resize.w, event.resize.h)) return false;
+            }
+            break;
+        case SDL_QUIT:
+            return false;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return true;
 }
 
 RGBAPI bool rgb_pwm(unsigned short r, unsigned short g, unsigned short b) {
-    if SDL_MUSTLOCK(screen) {
-        SDL_LockSurface(screen);
-    }
-    SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format,
-                                          pwm2srgb256(r),
-                                          pwm2srgb256(g),
-                                          pwm2srgb256(b)));
-    if SDL_MUSTLOCK(screen) {
-        SDL_UnlockSurface(screen);
-    }
-    SDL_UpdateRect(screen, 0, 0, width, height);
-    return !pollevents();
+    savedr = pwm2srgb256(r);
+    savedg = pwm2srgb256(g);
+    savedb = pwm2srgb256(b);
+    return redraw() && pollevents();
 }
 
 RGBAPI void rgb_close(void) {
