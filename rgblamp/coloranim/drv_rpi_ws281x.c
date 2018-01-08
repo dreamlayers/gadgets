b3f4144a69c61332e3953f3f621d46d0d98bd11b
Boris Gjenero <boris.gjenero@gmail.com>
2018-01-07 19:22:19 -0500

    Add LED strip power supply switching via wiringPi

--- rgblamp/coloranim/drv_rpi_ws281x.c
+++ rgblamp/coloranim/drv_rpi_ws281x.c
@@ -2,8 +2,13 @@
 #include <math.h>
 /* stdint.h required for ws2811.h */
 #include <stdint.h>
+#include <unistd.h>
+#include <wiringPi.h>
 #include "coloranim.h"
 #include "ws2811.h"
+
+/* GPIO for switching power to LED strip power supply */
+#define POWER_GPIO 24
 
 //#define ARRAY_SIZE(stuff)       (sizeof(stuff) / sizeof(stuff[0]))
 
@@ -51,6 +56,14 @@ void render_open()
         fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
         fatal("failed to open device");
     }
+
+    /* Power supply switching via GPIO */
+    wiringPiSetupGpio();
+    pinMode(POWER_GPIO, OUTPUT);
+    if (digitalRead(POWER_GPIO) == 0) {
+        digitalWrite(POWER_GPIO, 1);
+        usleep(1000000);
+    }
 }
 
 static unsigned short srgb2pwm(double n) {
@@ -83,5 +96,17 @@ void render(const pixel pix)
 
 void render_close(void)
 {
+    int i, allblack = 1;
+    for (i = 0; i < LED_COUNT; i++) {
+        if (ledstring.channel[0].leds[i] != 0) {
+            allblack = 0;
+            break;
+        }
+    }
+
     ws2811_fini(&ledstring);
+
+    if (allblack) {
+        digitalWrite(POWER_GPIO, 0);
+    }
 }
