566fb519b63f638b421c0e93cfa76ec7423de299
Boris Gjenero <boris.gjenero@gmail.com>
2024-10-16 00:08:10 -0400

    Handle signals and quit gracefully in standalone coloranim
    Killing coloranim while it is sending data to the Arduino
    controlled LED strip can leave the LED strip waiting for more data.
    This avoids that problem.

--- rgblamp/coloranim/standalone.c
+++ rgblamp/coloranim/standalone.c
@@ -1,11 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#ifndef WIN32
+#include <string.h>
+#include <signal.h>
+#endif
 #include "coloranim.h"
 
 /* --- command line argument parser --- */
 
 static const char **sargv;
 static unsigned int argidx, sargc;
+
+#ifndef WIN32
+static int quit = 0;
+
+static void sig_quit(int sig) {
+    quit = 1;
+}
+#endif /* !WIN32 */
 
 static const char *parse_args_getnext(void)
 {
@@ -42,10 +54,13 @@ static int parse_args(int argc, char **argv)
     return parse_and_run();
 }
 
-/* TODO catch signals and quit in response */
 int cmd_cb_pollquit(void)
 {
+#ifdef WIN32
     return 0;
+#else
+    return quit;
+#endif
 }
 
 void coloranim_notify(void)
@@ -55,6 +70,16 @@ void coloranim_notify(void)
 /* Standalone command line coloranim */
 int main(int argc, char **argv)
 {
+    struct sigaction sa;
+
+    memset (&sa, 0, sizeof(sa));
+    sa.sa_handler = sig_quit;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGHUP, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
     render_open();
     coloranim_init();
     parse_init();
